35b0b7c8bd44b86be31c4d0046141c6064b373cd
Daiani34 <88406830+Daiani34@users.noreply.github.com>
2022-09-24 14:37:48 -0300

    Adcionando sensor de presenca

--- projeto_luzinhas1.ino
+++ projeto_luzinhas1.ino
@@ -2,6 +2,8 @@
 //
 void setup()
 {
+  pinMode(7, INPUT);
+  Serial.begin(9600);
   pinMode(13, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(5, OUTPUT);
@@ -12,16 +14,19 @@ void loop()
 {
   int a[4] = {2,5,8,13};            //Definindo as portas dos LEDS
   int i = 0;                        //Controle do Index do Array
-  
-  while(true) {
-  	int led = a[i];                 //Identifica qual LED acender
-    digitalWrite(led, HIGH);        //Liga o LED
-    delay(1000);                    //Delay 
-    digitalWrite(led, LOW);         //Desliga o LED
-    i = i + 1;                      //Incrementa o valor para trocar de LED na proxima iteracao
+  int j = 0;                        //Controle de sequencia que se acendem os leds
+  if (digitalRead(7) == 1) {        //Se o sensor de presença foi acionado (0 não acionado, 1 acionado)
+    while(j < 5) {
+      int led = a[i];               //Identifica qual LED acender
+      digitalWrite(led, HIGH);      //Liga o LED
+      delay(1000);                  //Delay 
+      digitalWrite(led, LOW);       //Desliga o LED
+      i = i + 1;                    //Incrementa o valor para trocar de LED na proxima iteracao
+      if(i == 4) {        
+        i = 0;
+        j = j + 1;                  //Depois de cada sequencia, incrementa o valor da sequencia que se acendem os leds
+      } // end if
+    } // end while
+  } // end if
+} // end loop
 
-    if (i == 4) {                   //Verifica se eh o ultimo elemento do array
-      i=0;                          //Se for o ultimo elemento do array  atribui o valor do elemento inicial
-    }
-  }
-}
