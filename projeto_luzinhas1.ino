50ffb9e9f1ed217f841535a00d57c66f62857e6e
Daiani34 <88406830+Daiani34@users.noreply.github.com>
2022-09-24 12:27:58 -0300

    comentarios no codigo

--- projeto_luzinhas1.ino
+++ projeto_luzinhas1.ino
@@ -10,17 +10,18 @@ void setup()
 
 void loop()
 {
-  int a[4] = {2,5,8,13};
-  int i = 0;
+  int a[4] = {2,5,8,13};            //Definindo as portas dos LEDS
+  int i = 0;                        //Controle do Index do Array
   
   while() {
-  	int led = a[i];
-    digitalWrite(led, HIGH);
-    delay(1000);
-    digitalWrite(led, LOW);
-    i = i + 1;
-    if(i == 4) {
-      i=0;
+  	int led = a[i];                 //Identifica qual LED acender
+    digitalWrite(led, HIGH);        //Liga o LED
+    delay(1000);                    //Delay 
+    digitalWrite(led, LOW);         //Desliga o LED
+    i = i + 1;                      //Incrementa o valor para trocar de LED na proxima iteracao
+
+    if (i == 4) {                   //Verifica se eh o ultimo elemento do array
+      i=0;                          //Se for o ultimo elemento do array  atribui o valor do elemento inicial
     }
   }
 }
